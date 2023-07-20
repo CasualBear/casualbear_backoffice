92a713cb8bd893e460f913dea9cc20da09725cf8
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2023-07-20 13:14:02 +0100

    Update team_item.dart

--- lib/screens/events/widgets/team_item.dart
+++ lib/screens/events/widgets/team_item.dart
@@ -4,19 +4,23 @@ class TeamItem extends StatelessWidget {
   final String name;
 
   const TeamItem({
-    super.key,
+    Key? key,
     required this.name,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         // Add your logic to handle team card tap
+        // Navigate to the team details screen
       },
       child: Card(
         color: Colors.grey[200],
-        child: ListTile(title: Text(name)),
+        child: ListTile(
+          title: Text(name),
+          trailing: const Icon(Icons.arrow_forward),
+        ),
       ),
     );
   }
