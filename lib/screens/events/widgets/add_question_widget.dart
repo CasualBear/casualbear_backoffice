1c90d819ef8085c4f6deee7ff219b4408e077954
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2023-09-13 22:56:43 +0300

    Update add_question_widget.dart

--- lib/screens/events/widgets/add_question_widget.dart
+++ lib/screens/events/widgets/add_question_widget.dart
@@ -97,207 +97,209 @@ class AddQuestionDialogState extends State<AddQuestionDialog> {
       title: const Text('Adicionar Questão'),
       content: Form(
         key: _formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Visibility(
-              visible: !_isFake,
-              child: TextFormField(
-                controller: _questionController,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Visibility(
+                visible: !_isFake,
+                child: TextFormField(
+                  controller: _questionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Pergunta',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Introduza uma pergunta';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _locationController,
                 decoration: const InputDecoration(
-                  labelText: 'Pergunta',
+                  labelText: 'Localização',
+                ),
+                onTap: () async {
+                  await Navigator.push<String?>(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MapScreen(
+                        onAddressPicked: (latitude, longitude, address) {
+                          questionCoordinates = LatLong(latitude, longitude);
+                          _locationController.text = address;
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  );
+                },
+                readOnly: true,
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _pointsController,
+                decoration: const InputDecoration(
+                  labelText: 'Pontos',
                 ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Introduza uma pergunta';
+                    return 'Introduza pontos';
                   }
                   return null;
                 },
               ),
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _locationController,
-              decoration: const InputDecoration(
-                labelText: 'Localização',
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: _imageController,
+                decoration: const InputDecoration(
+                  labelText: 'Imagem',
+                ),
               ),
-              onTap: () async {
-                await Navigator.push<String?>(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MapScreen(
-                      onAddressPicked: (latitude, longitude, address) {
-                        questionCoordinates = LatLong(latitude, longitude);
-                        _locationController.text = address;
-                        Navigator.pop(context);
+              Row(
+                children: [
+                  const Text(
+                    'Zona: ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: selectedZone,
+                      onChanged: (value) {
+                        setState(() {
+                          selectedZone = value;
+                        });
+                      },
+                      items: zones.map((zone) {
+                        return DropdownMenuItem(
+                          value: zone,
+                          child: Text(zone),
+                        );
+                      }).toList(),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Visibility(
+                visible: !(_isFake || _isInvisible),
+                child: Row(
+                  children: [
+                    const Text('É desafio'),
+                    Switch(
+                      value: _isChallenge,
+                      onChanged: (value) {
+                        setState(() {
+                          _isChallenge = value;
+                        });
                       },
                     ),
-                  ),
-                );
-              },
-              readOnly: true,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _pointsController,
-              decoration: const InputDecoration(
-                labelText: 'Pontos',
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Introduza pontos';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: _imageController,
-              decoration: const InputDecoration(
-                labelText: 'Imagem',
-              ),
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Zona: ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  ],
                 ),
-                Expanded(
-                  child: DropdownButtonFormField(
-                    value: selectedZone,
-                    onChanged: (value) {
-                      setState(() {
-                        selectedZone = value;
-                      });
-                    },
-                    items: zones.map((zone) {
-                      return DropdownMenuItem(
-                        value: zone,
-                        child: Text(zone),
-                      );
-                    }).toList(),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Visibility(
-              visible: !(_isFake || _isInvisible),
-              child: Row(
-                children: [
-                  const Text('É desafio'),
-                  Switch(
-                    value: _isChallenge,
-                    onChanged: (value) {
-                      setState(() {
-                        _isChallenge = value;
-                      });
-                    },
-                  ),
-                ],
               ),
-            ),
-            const SizedBox(height: 10),
-            Visibility(
-              visible: !(_isChallenge || _isInvisible),
-              child: Row(
-                children: [
-                  const Text('É pergunta falsa?'),
-                  Switch(
-                    value: _isFake,
-                    onChanged: (value) {
-                      _questionController.clear();
-                      setState(() {
-                        _isFake = value;
-                      });
-                    },
-                  ),
-                ],
+              const SizedBox(height: 10),
+              Visibility(
+                visible: !(_isChallenge || _isInvisible),
+                child: Row(
+                  children: [
+                    const Text('É pergunta falsa?'),
+                    Switch(
+                      value: _isFake,
+                      onChanged: (value) {
+                        _questionController.clear();
+                        setState(() {
+                          _isFake = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Visibility(
-              visible: !(_isFake || _isChallenge),
-              child: Row(
-                children: [
-                  const Text('É pergunta Invisivel?'),
-                  Switch(
-                    value: _isInvisible,
-                    onChanged: (value) {
-                      setState(() {
-                        _isInvisible = value;
-                      });
-                    },
-                  ),
-                ],
+              const SizedBox(height: 10),
+              Visibility(
+                visible: !(_isFake || _isChallenge),
+                child: Row(
+                  children: [
+                    const Text('É pergunta Invisivel?'),
+                    Switch(
+                      value: _isInvisible,
+                      onChanged: (value) {
+                        setState(() {
+                          _isInvisible = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Visibility(
-              visible: !(_isFake || _isChallenge),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 8),
-                  const Text('Create answers:', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 8),
-                  ..._answerControllers.asMap().entries.map((entry) {
-                    final int index = entry.key;
-                    final controller = entry.value;
-                    return Row(
-                      children: [
-                        Flexible(
-                          flex: 4,
-                          child: TextFormField(
-                            controller: controller,
-                            decoration: const InputDecoration(
-                              labelText: 'Answer',
+              const SizedBox(height: 10),
+              Visibility(
+                visible: !(_isFake || _isChallenge),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    const Text('Create answers:', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 8),
+                    ..._answerControllers.asMap().entries.map((entry) {
+                      final int index = entry.key;
+                      final controller = entry.value;
+                      return Row(
+                        children: [
+                          Flexible(
+                            flex: 4,
+                            child: TextFormField(
+                              controller: controller,
+                              decoration: const InputDecoration(
+                                labelText: 'Answer',
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter an answer';
+                                }
+                                return null;
+                              },
                             ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter an answer';
-                              }
-                              return null;
+                          ),
+                          if (_answerControllers.length > 1 && index > 0)
+                            IconButton(
+                              onPressed: () {
+                                _removeAnswerField(index);
+                              },
+                              icon: const Icon(Icons.remove_circle),
+                              color: Colors.red,
+                              disabledColor: Colors.grey,
+                              constraints: const BoxConstraints(),
+                            ),
+                          if (index == _answerControllers.length - 1)
+                            IconButton(
+                              onPressed: () {
+                                _addAnswerField();
+                              },
+                              icon: const Icon(Icons.add_circle),
+                              color: Colors.green,
+                            ),
+                          Radio<int>(
+                            value: index,
+                            groupValue: _correctAnswerIndex,
+                            onChanged: (newValue) {
+                              setState(() {
+                                _correctAnswerIndex = newValue!;
+                              });
                             },
                           ),
-                        ),
-                        if (_answerControllers.length > 1 && index > 0)
-                          IconButton(
-                            onPressed: () {
-                              _removeAnswerField(index);
-                            },
-                            icon: const Icon(Icons.remove_circle),
-                            color: Colors.red,
-                            disabledColor: Colors.grey,
-                            constraints: const BoxConstraints(),
-                          ),
-                        if (index == _answerControllers.length - 1)
-                          IconButton(
-                            onPressed: () {
-                              _addAnswerField();
-                            },
-                            icon: const Icon(Icons.add_circle),
-                            color: Colors.green,
-                          ),
-                        Radio<int>(
-                          value: index,
-                          groupValue: _correctAnswerIndex,
-                          onChanged: (newValue) {
-                            setState(() {
-                              _correctAnswerIndex = newValue!;
-                            });
-                          },
-                        ),
-                        const Text('Correct Answer'),
-                      ],
-                    );
-                  }),
-                ],
+                          const Text('Correct Answer'),
+                        ],
+                      );
+                    }),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       actions: [
